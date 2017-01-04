e0918e770db5867311a830f93ec6e454cc49bd0a
Ben Sullivan <bensullivan@bens-mbp>
2017-01-04 10:20:19 +0000

    Initial Commit

---
+++ Custom Cells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Cells
+//
+//  Created by Ben Sullivan on 04/01/2017.
+//  Copyright © 2017 Ben Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
